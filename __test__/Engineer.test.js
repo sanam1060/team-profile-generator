941562ab93cecefa841586a385e3b5c3b240e933
sanam1060 <sanam_g1990@yahoo.com>
2022-11-30 21:46:18 -0500

    add engineer tests and methods

--- __test__/Engineer.test.js
+++ __test__/Engineer.test.js
@@ -1,1 +1,26 @@
-class Engineer extends Employee {}
+const Engineer = require("../lib/Engineer");
+const Employee = require("../lib/Employee");
+const { test } = require("@jest/globals");
+
+test("creates an engineer object", () => {
+  const engineer = new Engineer("Engineer1");
+
+  expect(typeof engineer).toBe("object");
+});
+
+test("gets github username", () => {
+  const engineer = new Engineer(
+    "Manager",
+    "1",
+    "sanamtest@gmail.com",
+    "Sanam1060"
+  );
+
+  expect(engineer.github).toBe("Sanamgithub");
+});
+
+test("getRole() returns Engineer", () => {
+  const engineer = new Engineer("Manager1");
+
+  expect(engineer.getRole()).toBe("Engineer");
+});
